638869789039a67442511bf3bf8374af973538c4
Andy Awidarto <andy.awidarto@gmail.com>
2013-10-03 23:05:37 +0700

    first push

---
+++ app/routes.php
@@ -0,0 +1,89 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make('hello');
+});
+
+Route::get('hashme/{mypass}',function($mypass){
+
+    print Hash::make($mypass);
+});
+
+Route::get('media',function(){
+    $media = Product::all();
+
+    print $media->toJson();
+
+});
+
+Route::get('login',function(){
+    return View::make('login');
+});
+
+Route::post('login',function(){
+
+    // validate the info, create rules for the inputs
+    $rules = array(
+        'email'    => 'required|email',
+        'password' => 'required|alphaNum|min:3'
+    );
+
+    // run the validation rules on the inputs from the form
+    $validator = Validator::make(Input::all(), $rules);
+
+    // if the validator fails, redirect back to the form
+    if ($validator->fails()) {
+        return Redirect::to('login')->withErrors($validator);
+    } else {
+
+        // find the user
+        $user = User::where('username', '=', Input::get('email'))->first();
+
+
+        // check if user exists
+        if ($user) {
+
+            // check if password is correct
+            if (Hash::check(Input::get('password'), $user->password)) {
+
+                // login the user
+                Auth::login($user);
+
+                return Redirect::to('/');
+
+            } else {
+                // validation not successful
+                // send back to form with errors
+                // send back to form with old input, but not the password
+                return Redirect::to('login')
+                    ->withErrors($validator)
+                    ->withInput(Input::except('password'));
+            }
+
+        } else {
+            // user does not exist in database
+            // return them to login with message
+            Session::flash('loginError', 'This user does not exist.');
+            return Redirect::to('login');
+        }
+
+    }
+
+});
+
+Route::get('logout',function(){
+    Auth::logout();
+    return Redirect::to('/');
+});
