c4b239aa8333ecd3696b5b3bceea272d2cb8b9ec
Andy Awidarto <andy.awidarto@gmail.com>
2014-06-24 13:32:48 +0700

    user login

--- app/views/partials/twitter.blade.php
+++ app/views/partials/twitter.blade.php
@@ -2,17 +2,37 @@
 <div id="twitbox" class="lionbar">
     <?php
 
-    $toimoitwit = Twitter::getSearch(array('q'=>'toimoi'));
+    //$toimoitwit = Twitter::getSearch(array('q'=>'toimoiindonesia'));
+
+    $toimoitwit = Twitter::getUserTimeline(array('screen_name'=>'toimoiindonesia'));
 
     ?>
     <ul>
-        @foreach($toimoitwit->statuses as $twit)
+
+        @foreach($toimoitwit as $twit)
         <li>
             <h6>{{ '@'.$twit->user->screen_name }}</h6>
             <p>
-                {{ $twit->text }}
+                {{ makeUrl($twit->text) }}
             </p>
         </li>
         @endforeach
     </ul>
 </div>
+
+<?php
+    function makeUrl($text){
+        $text = explode(' ',$text);
+        $new = array();
+        foreach($text as $w){
+            if(preg_match('/^http/', $w)){
+                $w = '<a href="'.$w.'">'.$w.'</a>';
+            }
+
+            $new[] = $w;
+        }
+
+        return implode(' ',$new);
+    }
+
+?>
