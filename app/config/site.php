91ec2ca0d470fd379bf84ee552e91aca5b786c22
Andy Awidarto <andy.awidarto@gmail.com>
2014-10-08 15:42:09 +0700

    refresh

--- app/config/site.php
+++ app/config/site.php
@@ -3,6 +3,6 @@
 return array(
     'design'=>'Design & Development by Jempol Digital',
     'name'=>'Toimoi Art Design',
-    'outlet_id'=>'53563e12ccae5b180200001f',
-    'outlet_tag'=>'Toimoi Webshop'
+    'outlet_id'=>'535721b68dfa196521000003',
+    'outlet_tag'=>'Toimoi Online Website'
     );
