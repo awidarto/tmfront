ccf58c9f003de5e76cbea96148189242effafae2
Andy Awidarto <andy.awidarto@gmail.com>
2015-05-12 16:24:08 +0700

    title normal

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.signin')
+@extends('layout.front')
 
 @section('content')
             <!-- if there are login errors, show them here -->
@@ -9,35 +9,46 @@
     </p>
 @endif
 
-{{ Form::open(array('url' => 'login','class'=>'form-signin')) }}
-        <h2 class="form-signin-heading">Please sign in</h2>
-        @if (Session::has('login_errors'))
-            @if (Session::get('loginError'))
-            <div class="alert alert-danger">{{ Session::get('loginError') }}</div>
-                 <button type="button" class="close" data-dismiss="alert"></button>
-                 Email or password incorrect.
+<div class="row">
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+
+    </div>
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+        {{ Form::open(array('url' => 'login','class'=>'form-signin')) }}
+            <h2 class="form-signin-heading">Please sign in</h2>
+            @if (Session::has('login_errors'))
+                @if (Session::get('loginError'))
+                <div class="alert alert-danger">{{ Session::get('loginError') }}</div>
+                     <button type="button" class="close" data-dismiss="alert"></button>
+                     Email or password incorrect.
+                @endif
             @endif
-        @endif
-        <p>
-            {{ $errors->first('email') }}
-            {{ $errors->first('password') }}
-        </p>
+            <p>
+                {{ $errors->first('email') }}
+                {{ $errors->first('password') }}
+            </p>
 
-        <p>
-            {{ Form::label('email', 'Email Address') }}
-            {{ Form::text('email', Input::old('email'), array('placeholder' => 'awesome@awesome.com')) }}
-        </p>
-        <p>
-            {{ Form:: label('password', 'Password') }}
-            {{ Form::password('password') }}
-        </p>
+            <p>
+                {{ Form::label('email', 'Email Address') }}
+                {{ Form::text('email', Input::old('email'), array('placeholder' => 'awesome@awesome.com','class'=>'form-control col-md-8 ')) }}
+            </p>
+            <p>
+                {{ Form:: label('password', 'Password') }}
+                {{ Form::password('password', array('class'=>'form-control col-md-8') ) }}
+            </p>
 
-        <label class="checkbox">
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
+            <br />
+            <br />
+            <p>
+                {{ Form::submit('Login',array('class'=>'btn btn-primary')) }}
+            </p>
+        {{ Form::close() }}
 
+    </div>
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
 
-    <p>{{ Form::submit('Submit!',array('class'=>'btn btn-primary')) }}</p>
-{{ Form::close() }}
+    </div>
+</div>
+
 
 @stop
