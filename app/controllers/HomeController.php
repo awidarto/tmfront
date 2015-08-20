34490d8d5d266ad398cde420162cd7b985794d09
Andy Awidarto <andy.awidarto@gmail.com>
2015-08-20 16:29:28 +0700

    homeproduct

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -59,7 +59,7 @@ class HomeController extends BaseController {
         $products = Product::where('tags','like','%home%')
                             ->where('status','active')
                             ->orderBy('category','desc')
-                            ->orderBy('createdAt','desc')
+                            ->orderBy('createdAt','asc')
                             //->take(8)
                             ->get()->toArray();
 
