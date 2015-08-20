e56ede12f3de1bd2b477cc522ee45b26c999d971
Andy Awidarto <andy.awidarto@gmail.com>
2015-08-20 14:53:22 +0700

    amt

--- app/views/partials/location.blade.php
+++ app/views/partials/location.blade.php
@@ -2,11 +2,11 @@
     <div class="col-md-11 pull-right" id="tm-location">
         <h6>COME AND VISIT TOIMOI</h6>
         <p>
-            The Mansion Kemang GF15<br />
+            The Mansion at Kemang<br />Unit GF15<br />
             Jl Kemang Raya no. 3-5<br />
             Jakarta Selatan 12730<br />
             Indonesia<br />
-            p. +62 21 29055525&nbsp;
+            p. +62 21 29055525<br />
             f. +62 21 29055526
         </p>
         <p>
