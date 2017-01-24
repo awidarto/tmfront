c885e91a5a8665eeba621fee0d127a606b630e3f
muhamad sahlan <alan.alenyor@gmail.com>
2017-01-24 11:29:07 +0700

    db

--- app/config/database.php
+++ app/config/database.php
@@ -50,8 +50,8 @@ return array(
             'driver'   => 'mongodb',
             'host'     => 'localhost',
             'port'     => 27017,
-            'username' => 'shoplite',
-            'password' => 'shoplit3r',
+            'username' => 'shopli',
+            'password' => 'shoplit3',
             'database' => 'toimoi'
         ),
 
